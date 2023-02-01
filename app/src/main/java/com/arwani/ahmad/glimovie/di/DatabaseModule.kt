22450e7a7788253f37d06e14c5672f9b3d0a0a0d
arwani <arwaniahmad659@gmail.com>
2023-02-01 14:56:14 +0700

    Database Module

--- app/src/main/java/com/arwani/ahmad/glimovie/di/DatabaseModule.kt
+++ app/src/main/java/com/arwani/ahmad/glimovie/di/DatabaseModule.kt
@@ -1,4 +1,37 @@
 package com.arwani.ahmad.glimovie.di
 
+import android.content.Context
+import androidx.room.Room
+import com.arwani.ahmad.glimovie.data.local.room.MoviesDao
+import com.arwani.ahmad.glimovie.data.local.room.MoviesDatabase
+import com.arwani.ahmad.glimovie.data.local.room.RemoteKeysDao
+import com.arwani.ahmad.glimovie.utils.Constant
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
 class DatabaseModule {
+
+    @Singleton
+    @Provides
+    fun provideDatabase(@ApplicationContext context: Context): MoviesDatabase {
+        return Room.databaseBuilder(
+            context,
+            MoviesDatabase::class.java,
+            Constant.MOVIE_DB
+        ).fallbackToDestructiveMigration().build()
+    }
+
+    @Singleton
+    @Provides
+    fun provideMoviesDao(moviesDatabase: MoviesDatabase): MoviesDao = moviesDatabase.getMoviesDao()
+
+    @Singleton
+    @Provides
+    fun provideRemoteKeysDao(moviesDatabase: MoviesDatabase): RemoteKeysDao = moviesDatabase.getRemoteKeysDao()
 }
--- app/src/main/java/com/arwani/ahmad/glimovie/utils/Constant.kt
+++ app/src/main/java/com/arwani/ahmad/glimovie/utils/Constant.kt
@@ -2,4 +2,5 @@ package com.arwani.ahmad.glimovie.utils
 
 object Constant {
     const val BASE_URL = "https://api.themoviedb.org/3/"
+    const val MOVIE_DB = "movies_database"
 }
